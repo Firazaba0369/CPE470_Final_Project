c339eb0b7f9792c07e84da9250a7213eed08cfe3
Dakshesh <dpasala@calpoly.edu>
2026-05-21 18:29:05 -0700

    testing out commits

--- CPE470_Final_Project.ino
+++ CPE470_Final_Project.ino
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <TensorFlowLite.h>
+#include <TensorFlowLite.h> 
 
 #include "main_functions.h"
 
